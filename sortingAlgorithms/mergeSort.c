61c0b42d7a7b537488d349a8eb44d6305cf2d65e
Ege-Guler <93088582+Ege-Guler@users.noreply.github.com>
2022-06-04 12:17:46 +0300

    Update mergeSort.c

--- sortingAlgorithms/mergeSort.c
+++ sortingAlgorithms/mergeSort.c
@@ -5,96 +5,101 @@
     04/22/2022
 */
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
+#include <stdlib.h>
 
-#define N 10
 
-void merge(int* arr, int h, int m, int l)
-{
-    int lenFirst = m - l + 1;
-    int lenSecond = h - m;
-    
-    int tmp1[lenFirst];
-    int tmp2[lenSecond];
 
-    for(int i = 0; i < lenFirst; i++)
-    {
-        tmp1[i] = arr[i + l];
-    }
-    for(int i = 0; i < lenSecond; i++)
-    {
-        tmp2[i] = arr[m +1 + i];
-    }
-
-    int x = 0, y = 0, z = l;
-
-    while(x < lenFirst && y < lenSecond)
-    {
-        if(tmp1[x] < tmp2[y])
-        {
-            arr[z] = tmp1[x];
-            x++;
-        }
-        else
-        {
-            arr[z] = tmp2[y];
-            y++;
-        }
-        z++;
-    }
-
-    while(x < lenFirst)
-    {
-        arr[z] = tmp1[x];
-        x++;
-        z++;
-    }
-    
-    while(y < lenSecond)
-    {
-        arr[z] = tmp2[y];
-        y++;
-        z++;
-    }
-}
-
-void printArr(int* arr)
-{
-    for(int i = 0; i < N - 1; i++)
-    {
-        printf("%d - ", arr[i]);
-    }
-    printf("%d", arr[N - 1]);
-}
-
-void mergeSort(int* arr, int h, int l)
-{
-    if(h > l)
-    {
-        int m = (h + l) / 2;
-        mergeSort(arr, m, l);
-        mergeSort(arr, h, m + 1);
-        merge(arr, h, m, l);
-    }
-}
+void merge(int* arr, int l, int m, int h);
+void sort(int* arr, int h, int l);
+void printArr(int* arr, int len);
 
 
 
 int main(void)
 {
-    int arr[N];
-    srand(time(NULL)); 
-    for(int i = 0; i < N; i++)
+    srand(time(NULL));
+
+    int SIZE = rand() % 18 + 2;
+    printf("%d", SIZE);
+
+    int *arr = (int* )malloc(SIZE* sizeof(int));
+
+    for(int i = 0; i < SIZE; i++)
     {
-        arr[i] = rand() % 21;
+        arr[i] = rand() % 41 - 20;
     }
-    printf("\n----------------Unsorted Array----------------\n");
-    printArr(arr);
-    mergeSort(arr, 9, 0);
-    printf("\n----------------Sorted Array----------------\n");
-    printArr(arr);
 
+    
 
+    printf("\n---Unsorted Arr---\n");
+    printArr(arr, SIZE);
+    sort(arr, SIZE -1, 0);
+    printf("\n----Sorted Arr----\n");
+    printArr(arr, SIZE);
+
+    free(arr);
     return 0;
+}
+
+void merge(int* arr, int l, int m, int h)
+{
+
+    int lenFirst = m - l + 1;
+    int lenSecond = h - m;
+
+    int tempFirst[lenFirst];
+    int tempSecond[lenSecond];
+
+    for(int i = 0; i < lenFirst; i++)
+    {
+        tempFirst[i] = arr[i + l];
+    }
+
+    for(int i = 0; i < lenSecond; i++)
+    {
+        tempSecond[i] = arr[i + m + 1];
+    }
+
+    int i = 0, j = 0, k = l;
+
+    while(i < lenFirst && j < lenSecond)
+    {
+        if(tempFirst[i] < tempSecond[j])
+        {
+            arr[k] = tempFirst[i];
+            i++; 
+        }
+        else
+        {
+            arr[k] = tempSecond[j];
+            j++;
+        }
+        k++;
+    }
+
+    for(; i < lenFirst; arr[k] = tempFirst[i], i++, k++);
+    for(; i < lenSecond; arr[k] = tempSecond[i], j++, k++);
+
+}
+
+void sort(int* arr, int h, int l)
+{
+    if(h > l)
+    {
+        int m = (h + l) / 2;
+        sort(arr, m , l);
+        sort(arr, h, m + 1);
+        merge(arr, l, m, h);
+    }
+}
+
+void printArr(int* arr, int len)
+{
+
+    for(int i = 0; i < len - 1; i++)
+    {
+        printf("%d | ", arr[i]);
+    }
+    printf("%d", arr[len - 1]);
 }
